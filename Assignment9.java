9531e6bd7cad9f8f3d401433165d060b56a9941b
ganeshpunati <99862064+ganeshpunati@users.noreply.github.com>
2022-02-25 09:46:19 +0530

    Add files via upload

---
+++ Assignment9.java
@@ -0,0 +1,14 @@
+package Test;
+public class Assignment9 {
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=5;j>=i;j--) {
+			System.out.print(" ");
+	    	}
+		for(int j=i;j<=5;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	    }
+	}
+}
