07149832af1556efb9b375416315b6ebbc2f9c25
ganeshpunati <99862064+ganeshpunati@users.noreply.github.com>
2022-02-25 09:40:00 +0530

    Add files via upload

---
+++ LogicalThinking2.java
@@ -0,0 +1,18 @@
+package Assignment8;
+
+public class LogicalThinking2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=7;i++) {
+			for(int j=7-i+1;j<=7;j++) {
+				System.out.print(" ");
+		}
+			for(int j=i;j<=7;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
