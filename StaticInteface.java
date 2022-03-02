d505fbc0a64ea7e0e5a8ad8e0dbc2fc491e35648
ganeshpunati <99862064+ganeshpunati@users.noreply.github.com>
2022-03-02 10:10:21 +0530

    Update StaticInteface.java

--- StaticInteface.java
+++ StaticInteface.java
@@ -1,6 +1,10 @@
 package Assignment19;
 public interface StaticInteface {
+	static void  show(){
+		System.out.println("static show method");
+	}
 	static void demo() {
+		show();
 		System.out.println("Static demo method");
 	}
 }
