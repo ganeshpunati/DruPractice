c8d6c5eb7e2d41f4ecb18f9e229ad7fbf35e4618
ganeshpunati <99862064+ganeshpunati@users.noreply.github.com>
2022-03-22 15:01:05 +0530

    deleted

--- Assignment/src/main/java/com/example/demo/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, String>{
-
-	//findByName
-}
